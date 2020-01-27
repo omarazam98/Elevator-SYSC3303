d1b2a4a48f1cdb7ad796a0acb4b58cafa9c7bbb7
nkix <nrx0320@outlook.com>
2020-01-27 13:03:58 -0500

    Add files via upload
    add the requestType for request and lampStatus for button and direction lamp

---
+++ src/info/LampStatus.java
@@ -0,0 +1,28 @@
+package info;
+
+public enum LampStatus {
+	UP("UP"), DOWN("DOWN"), OFF("OFF");
+	
+	private String str;
+	
+	LampStatus(String str){
+		this.setStr(str);
+	}
+	
+	public LampStatus getStatus(String str) {
+		switch(str) {
+		case "UP": return UP;
+		case "DOWN": return DOWN;
+		default: return OFF;
+		}
+	}
+
+	public void setStr(String str) {
+		this.str = str;
+	}
+	
+	@Override
+	public String toString() {
+		return str;
+	}
+}
---
+++ src/info/RequestType.java
@@ -0,0 +1,5 @@
+package info;
+
+public enum RequestType {
+	FLOORDIRECTIONLAMP, FLOORBUTTON, ELEVATORARRIVAL; //for floor
+}
