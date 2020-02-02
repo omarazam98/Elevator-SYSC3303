438a9ea9158370a8fadc702ce3bdee945eabb72f
omarazam98 <umarazam98@gmail.com>
2020-02-01 20:08:33 -0500

    added tests

---
+++ src/info/Tests.java
@@ -0,0 +1,14 @@
+package info;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+@RunWith(Suite.class)
+@SuiteClasses({
+TestHelper.class,
+})
+
+public class Tests 
+{
+	
+}
